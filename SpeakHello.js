2e76dce18773596a263b8f34e4b21a3be03fe16b
mustifanci <mustifanci@gmail.com>
2021-11-08 22:04:45 +0300

    bitti

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,6 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function(window)) {
+(function(window) {
 
     // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
     // the "speak" method and which you will expose to the global context
@@ -23,4 +23,4 @@
     // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
     // xxxx.xxxx = helloSpeaker;
     window.helloSpeaker = helloSpeaker;
-}(window);
+})(window);
